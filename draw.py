1f9359791216870d54a2aeb02d7a111f78a13ddb
sotrh <bhbenjaminhansen@gmail.com>
2016-12-31 17:39:23 -0700

    removed draw.py as it is no longer necessary

--- draw.py
+++
@@ -1,6 +0,0 @@
-import pygame
-from snake import Snake
-
-def draw_snake(surface, snake, color, scale):
-	for x, y in snake._body:
-		pygame.draw.rect(surface, color, pygame.Rect(x * scale, y * scale, scale, scale))
